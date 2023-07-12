bf59ecc5e5329539d4f676c5ab17dfd96d62d61c
AnatoliUa <soyanatoli@hotmail.com>
2023-07-12 22:16:13 +0300

    Project task 2 done

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+class User:
+    def __init__(self) -> None:
+        self.name = None
+        self.second_name = None
+    def create(self):
+        self.name = "Anatoli"
+        self.second_name = "Muliar"
+    def remove(self):
+        self.name = ""
+        self.second_name = ""
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+    yield user
+    user.remove()
