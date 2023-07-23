b9c26037e4d5c838f8f1ea8dbab10e7b628997bc
AnatoliUa <soyanatoli@hotmail.com>
2023-07-23 14:09:01 +0300

    Project task 4 ind part done

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -11,3 +11,25 @@ class GitHub:
         body = r.json()
         return body
     
+    def people_user_follows(self):
+        token = 'Bearer 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'
+        hdr = {'Accept': 'application/vnd.github+json', 'Authorization': token}
+        #r = requests.get(f'https://api.github.com/user/following/{username}', {'Authorization': token})
+        r = requests.get(f'https://api.github.com/user/following', headers=hdr)
+        return r
+    
+    def emojis(self):
+        r = requests.get('https://api.github.com/emojis')
+        body = r.json()
+        return body
+    
+    def root(self):
+        r = requests.get('https://api.github.com/')
+        body = r.json()
+        return body
+    
+    def gitignore(self):
+        r = requests.get('https://api.github.com/gitignore/templates')
+        body = r.json()
+        return body
+    
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -24,4 +24,24 @@ def test_repo_cannot_be_found(github_api):
 def test_repo_with_single_char_be_found(github_api):
     r = github_api.search_repo('s')
     assert r['total_count'] != 0
+
+@pytest.mark.myapi
+def test_people_user_follows(github_api):
+    r = github_api.people_user_follows('defunkt')
+    assert r.status_code == 200
     
+@pytest.mark.myapi
+def test_emojis(github_api):
+    r = github_api.emojis()
+    assert 'ukraine' in r.keys()
+
+@pytest.mark.myapi
+def test_root(github_api):
+    r = github_api.root()
+    assert r["current_user_url"] == "https://api.github.com/user"
+
+@pytest.mark.myapi
+def test_root(github_api):
+    r = github_api.gitignore()
+    assert "Android" in r
+  
